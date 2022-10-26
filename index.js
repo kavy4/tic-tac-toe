37c0023418b211b3679705a86a75201f75aefc4b
kavy4 <109154341+kavy4@users.noreply.github.com>
2022-10-26 18:05:34 +0300

    Add files via upload

--- index.js
+++ index.js
@@ -4,76 +4,77 @@ let symbol = document.getElementsByClassName('symbol')
 
 let numMove = 1
 
+let point1 = 0
+
+let point2 = 0
+
 function CheckWinner()
 {
     let gameOver = false
-    let winPos
+    
     if (symbol[0].textContent != '' && symbol[0].textContent == symbol[1].textContent && symbol[1].textContent == symbol[2].textContent)
     {
         gameOver = true
-        winPos = 0
     }
     if (symbol[3].textContent != '' && symbol[3].textContent == symbol[4].textContent && symbol[4].textContent == symbol[5].textContent)
     {
         gameOver = true
-        winPos = 3
     }
     if (symbol[6].textContent != '' && symbol[6].textContent == symbol[7].textContent && symbol[7].textContent == symbol[8].textContent)
     {
         gameOver = true
-        winPos = 6
     }
     if (symbol[0].textContent != '' && symbol[0].textContent == symbol[3].textContent && symbol[3].textContent == symbol[6].textContent)
     {
         gameOver = true
-        winPos = 0
     }
     if (symbol[1].textContent != '' && symbol[1].textContent == symbol[4].textContent && symbol[4].textContent == symbol[7].textContent)
     {
         gameOver = true
-        winPos = 1
     }
     if (symbol[2].textContent != '' && symbol[2].textContent == symbol[5].textContent && symbol[5].textContent == symbol[8].textContent)
     {
         gameOver = true
-        winPos = 2
     }
     if (symbol[0].textContent != '' && symbol[0].textContent == symbol[4].textContent && symbol[4].textContent == symbol[8].textContent)
     {
         gameOver = true
-        winPos = 0
     }
     if (symbol[2].textContent != '' && symbol[2].textContent == symbol[4].textContent && symbol[4].textContent == symbol[6].textContent)
     {
         gameOver = true
-        winPos = 2
     }
 
     if (gameOver)
     {
-        if (symbol[winPos].textContent == 'close')
+        if (numMove % 2 != 0)
         {
-            alert('Player 1 won')
+            document.getElementById('win-player').textContent = 'Player 1 win!'
+            point1++
+            document.getElementById('player1-score').textContent = 'Player 1 score: ' + point1
         }
         else
         {
-            alert('Player 2 won')
-        }
-        CleanBoard()
+            document.getElementById('win-player').textContent = 'Player 2 win!'
+            point2++
+            document.getElementById('player2-score').textContent = 'Player 2 score: ' + point2
+        }   
     }
 }
 
-function CleanBoard()
+function RestartGame()
 {
     for (let el of symbol)
     {
         el.textContent = ''
     }
+    numMove = 1
+    document.getElementById('win-player').textContent = ''
 }
 
 document.getElementById('button-restart').addEventListener('click', () =>
 {
-    CleanBoard()
+    RestartGame()
 })
 
 
@@ -85,12 +86,12 @@ for (let index = 0; index < cells.length; index++)
         {
             if (numMove % 2 == 0)
             {
-                symbol[index].textContent = 'radio_button_unchecked'
+                symbol[index].textContent = 'X'
                 // circle
             }
             else
             {
-                symbol[index].textContent = 'close'
+                symbol[index].textContent = 'O'
                 // cross
             }
             numMove++
